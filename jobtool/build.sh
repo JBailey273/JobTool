fc2df8094ab05d8ca43047378f6674f02a10808e
JBailey273 <jbailey@squire.enterprises>
2025-08-27 22:19:44 -0400

    Update build.sh

--- jobtool/build.sh
+++ jobtool/build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
+set -o pipefail
+set -o nounset
 
+
+python -V
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install --no-cache-dir -r requirements.txt
 
-# Django static + DB setup
-python manage.py collectstatic --noinput
+
+# Run migrations before collectstatic so DB is ready
 python manage.py migrate --noinput --run-syncdb
+python manage.py collectstatic --noinput
