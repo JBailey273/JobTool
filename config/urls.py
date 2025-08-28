e0e2a8a96b9df29685a25f95d5a5f039663d9d75
JBailey273 <jbailey@squire.enterprises>
2025-08-28 01:27:40 -0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
+def root(request):
+    return HttpResponseRedirect("/login/") if not request.user.is_authenticated else HttpResponseRedirect("/admin/")
 
 urlpatterns = [
-path("admin/", admin.site.urls),
-path("", include("core.urls")),
+    path("", root, name="root"),
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
 ]
