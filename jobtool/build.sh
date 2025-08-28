106616aa49b5ae8d893fbb9e240d1d7a45acbf25
JBailey273 <jbailey@squire.enterprises>
2025-08-27 22:04:40 -0400

    Delete jobtool directory

--- jobtool/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-
-# Django static + DB setup
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput --run-syncdb
--- jobtool/config/urls.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
-]
