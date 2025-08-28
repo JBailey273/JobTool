177ebdee50b898a748522c585767a254e0bae04c
JBailey273 <jbailey@squire.enterprises>
2025-08-27 22:47:08 -0400

    Delete jobtool directory

--- jobtool/core/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-from django.apps import AppConfig
-
-class CoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
-    verbose_name = "Job Tool"
