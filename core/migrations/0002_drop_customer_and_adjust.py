221471c1b783bc785d44f49ac4ad83a75db13855
JBailey273 <jbailey@squire.enterprises>
2025-08-28 00:08:29 -0400

    Update 0002_drop_customer_and_adjust.py

--- core/migrations/0002_drop_customer_and_adjust.py
+++ core/migrations/0002_drop_customer_and_adjust.py
@@ -1,1 +1,39 @@
+from django.db import migrations, models
 
+
+class Migration(migrations.Migration):
+dependencies = [
+('core', '0001_initial'),
+]
+
+
+operations = [
+migrations.AlterUniqueTogether(
+name='client',
+unique_together=set(),
+),
+migrations.AlterUniqueTogether(
+name='project',
+unique_together=set(),
+),
+migrations.RemoveField(
+model_name='client',
+name='customer',
+),
+migrations.RemoveField(
+model_name='project',
+name='customer',
+),
+migrations.AlterField(
+model_name='client',
+name='name',
+field=models.CharField(max_length=200, unique=True),
+),
+migrations.AlterUniqueTogether(
+name='project',
+unique_together={('client', 'name')},
+),
+migrations.DeleteModel(
+name='Customer',
+),
+]
